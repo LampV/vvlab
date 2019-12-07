e6f4025bd6d148a8c325e58c6f628f907370f56d
LampV <1071952403@qq.com>
2019-12-07 21:44:33 +0800

    models中新增DQN

--- wjwgym-home/wjwgym/agents/__init__.py
+++ wjwgym-home/wjwgym/agents/__init__.py
@@ -3,9 +3,10 @@
 """
 @author: Jiawei Wu
 @create time: 2019-12-06 23:16
-@edit time: 2019-12-06 23:17
+@edit time: 2019-12-07 20:28
 @file: /wjwgym-home/wjwgym/agents/__init__.py
 """
 
 
-from DDPG_base import DDPGBase
+from wjwgym.agents.DDPG_base import DDPGBase
+from wjwgym.agents.DQN_base import DQNBase
