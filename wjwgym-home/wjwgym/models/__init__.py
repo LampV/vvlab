9c060cd23158d8559d5b835a9a44997fec60d9d4
LampV <1071952403@qq.com>
2019-12-07 21:44:55 +0800

    将使用的简单神经网络抽象为Simple网络

---
+++ wjwgym-home/wjwgym/models/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+@author: Jiawei Wu
+@create time: 2019-12-06 23:23
+@edit time: 2019-12-07 20:16
+@file: /wjwgym-home/wjwgym/net_models/__init__.py
+"""
+
+
+from wjwgym.models.actor_net import SimpleActorNet
+from wjwgym.models.critic_net import SimpleCriticNet
+from wjwgym.models.dqn_net import SimpleDQNNet
