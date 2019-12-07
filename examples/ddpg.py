ceaf495e2a869da070cdf98965e2c498b840d495
LampV <1071952403@qq.com>
2019-12-07 21:43:47 +0800

    将网络抽出，纯净代码

--- examples/ddpg.py
+++ examples/ddpg.py
@@ -3,93 +3,26 @@
 """
 @author: Jiawei Wu
 @create time: 2019-12-06 23:01
-@edit time: 2019-12-06 23:13
+@edit time: 2019-12-07 21:39
 @file: /test.py
 """
 
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import numpy as np
 import time
 import gym
-from wjwgym import DDPGBase
+from wjwgym.agents import DDPGBase
+from wjwgym.models import SimpleActorNet, SimpleCriticNet
 CUDA = torch.cuda.is_available()
-
-
-class Anet(nn.Module):
-    """定义Actor的网络结构"""
-
-    def __init__(self, n_states, n_actions, a_bound):
-        """
-        定义隐藏层和输出层参数
-        @param n_obs: number of observations
-        @param n_actions: number of actions
-        @param a_bound: bound of actino
-        """
-        super(Anet, self).__init__()
-        n_neurons = 32
-        self.fc1 = nn.Linear(n_states, n_neurons)
-        self.fc1.weight.data.normal_(0, 0.1)
-        self.out = nn.Linear(n_neurons, n_actions)
-        self.out.weight.data.normal_(0, 0.1)
-        if CUDA:
-            self.bound = torch.FloatTensor(a_bound).cuda()
-        else:
-            self.bound = torch.FloatTensor(a_bound)
-
-    def forward(self, x):
-        """
-        定义网络结构: 第一层网络->ReLU激活->输出层->tanh激活->softmax->输出
-        """
-        x = x.cuda() if CUDA else x
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.out(x)
-        action_value = F.tanh(x)
-        action_value = action_value * self.bound
-        return action_value
-
-
-class Cnet(nn.Module):
-    """定义Critic的网络结构"""
-
-    def __init__(self, n_states, n_actions):
-        """
-        定义隐藏层和输出层参数
-        @param n_obs: number of observations
-        @param n_actions: number of actions
-        """
-        super(Cnet, self).__init__()
-        n_neurons = 30
-        self.fc_state = nn.Linear(n_states, n_neurons)
-        self.fc_state.weight.data.normal_(0, 0.1)
-        self.fc_action = nn.Linear(n_actions, n_neurons)
-        self.fc_action.weight.data.normal_(0, 0.1)
-        self.out = nn.Linear(n_neurons, 1)
-        self.out.weight.data.normal_(0, 0.1)
-
-    def forward(self, s, a):
-        """
-        定义网络结构: 
-        state -> 全连接   -·-->  中间层 -> 全连接 -> ReLU -> Q值
-        action -> 全连接  /相加，偏置
-        """
-        s, a = (s.cuda(), a.cuda()) if CUDA else (s, a)
-        x_s = self.fc_state(s)
-        x_a = self.fc_action(a)
-        x = F.relu(x_s+x_a)
-        actions_value = self.out(x)
-        return actions_value
 
 
 class DDPG(DDPGBase):
     def _build_net(self):
         n_states, n_actions = self.n_states, self.n_actions
-        self.actor_eval = Anet(n_states, n_actions, self.bound)
-        self.actor_target = Anet(n_states, n_actions, self.bound)
-        self.critic_eval = Cnet(n_states, n_actions)
-        self.critic_target = Cnet(n_states, n_actions)
+        self.actor_eval = SimpleActorNet(n_states, n_actions, a_bound=self.bound)
+        self.actor_target = SimpleActorNet(n_states, n_actions, a_bound=self.bound)
+        self.critic_eval = SimpleCriticNet(n_states, n_actions)
+        self.critic_target = SimpleCriticNet(n_states, n_actions)
 
 
 
