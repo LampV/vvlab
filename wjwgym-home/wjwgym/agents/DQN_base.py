df4b59b4ad8db0ebd87c5506e5d37a4e11cd2947
LampV <1071952403@qq.com>
2019-12-07 21:42:50 +0800

    将DQN代码抽象为DQNBase

--- wjwgym-home/wjwgym/agents/DQN_base.py
+++ wjwgym-home/wjwgym/agents/DQN_base.py
@@ -3,54 +3,20 @@
 """
 @author: Jiawei Wu
 @create time: 2019-11-17 11:23
-@edit time: 2019-12-06 22:51
+@edit time: 2019-12-07 20:26
 @file: /dqn.py
 @desc: 创建DQN对象
 """
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import numpy as np
 import pandas as pd
 import gym
-import wjwgym
-from functools import reduce
-from tqdm import trange
-from Utils import ExpReplay
-
+from wjwgym.agents.Utils import ExpReplay
 CUDA = torch.cuda.is_available()
 
 
-class Net(nn.Module):
-    """定义DQN的网络结构"""
-
-    def __init__(self, n_states, n_actions):
-        """
-        定义隐藏层和输出层参数
-        @param n_obs: number of observations
-        @param n_actions: number of actions
-        """
-        super(Net, self).__init__()
-        n_neurons = 32
-        self.fc1 = nn.Linear(n_states, n_neurons)
-        self.fc1.weight.data.normal_(0, 0.1)
-        self.out = nn.Linear(n_neurons, n_actions)
-        self.out.weight.data.normal_(0, 0.1)
-
-    def forward(self, x):
-        """
-        定义网络结构: 第一层网络->ReLU激活->输出层->softmax->输出
-        """
-        if CUDA:
-            x = x.cuda()
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.out(x)
-        action_values = F.softmax(x, dim=1)
-        return action_values
-
-
-class DQN(object):
+class DQNBase(object):
     def __init__(self, n_states, n_actions, learning_rate=0.01, discount_rate=0.9):
         """
         初始化DQN的两个网络和经验回放池
@@ -65,10 +31,9 @@ class DQN(object):
         self.epsilon_decay = 0.9
         self.eval_every = 10
         # 网络创建
-        self.n_states = n_states
-        self.n_actions = n_actions
-        self.eval_net = Net(n_states, n_actions)
-        self.target_net = Net(n_states, n_actions)
+        self.n_states, self.n_actions = n_states, n_actions
+        self._build_net()
+
         self.replay_buff = ExpReplay(n_states, 1, MAX_MEM=200)
         # 定义优化器和损失函数
         self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=learning_rate)
@@ -77,6 +42,9 @@ class DQN(object):
         self.eval_step = 0
         if CUDA:
             self.cuda()
+
+    def _build_net(self):
+        raise TypeError("Network build no implementation")
 
     def get_action(self, state):
         # epsilon update
@@ -127,43 +95,3 @@ class DQN(object):
     def cuda(self):
         self.eval_net.cuda()
         self.target_net.cuda()
-
-
-def rl_loop():
-    MAX_EPISODES = 200
-    env = gym.make('Maze-v0')
-    n_states = reduce(np.multiply, env.observation_space.shape)
-    n_actions = env.action_space.n
-    agent = DQN(n_states, n_actions)
-    # train
-    for ep in range(MAX_EPISODES):
-        cur_state = env.reset()
-        cur_state = cur_state.reshape((n_states))
-        done = False
-        while not done:
-            action = agent.get_action(cur_state)
-            next_state, reward, done, info = env.step(action)
-            next_state = next_state.reshape((n_states))
-            agent.add_step(cur_state, action, reward, done, next_state)
-            agent.learn()
-            cur_state = next_state
-        print('ep: ', ep, ' steps: ', info, ' final reward: ', reward)
-    print('done')
-    cur_state = env.reset()
-    cur_state = cur_state.reshape((n_states))
-    done = False
-
-    # test
-    while not done:
-        action = agent.get_action(cur_state)
-        action_values = agent.get_raw_out(cur_state)
-        print(action_values)
-        next_state, reward, done, info = env.step(action)
-        next_state = next_state.reshape((n_states))
-        agent.add_step(cur_state, action, reward, done, next_state)
-        agent.learn()
-        cur_state = next_state
-
-
-if __name__ == '__main__':
-    rl_loop()
