6f9cdccdb22e0214a5008ce55a2c03d1fa732a5c
LampV <1071952403@qq.com>
2020-01-15 16:16:27 +0800

    从相对路径import模型类

--- wjwgym-home/wjwgym/models/__init__.py
+++ wjwgym-home/wjwgym/models/__init__.py
@@ -3,11 +3,11 @@
 """
 @author: Jiawei Wu
 @create time: 2019-12-06 23:23
-@edit time: 2019-12-07 20:16
+@edit time: 2020-01-15 16:16
 @file: /wjwgym-home/wjwgym/net_models/__init__.py
 """
 
 
-from wjwgym.models.actor_net import SimpleActorNet
-from wjwgym.models.critic_net import SimpleCriticNet
-from wjwgym.models.dqn_net import SimpleDQNNet
+from .actor_net import SimpleActorNet
+from .critic_net import SimpleCriticNet
+from .dqn_net import SimpleDQNNet
