fac71423b031dba19cfb2a379f5e7486459f8827
wujiawei <1071952403@qq.com>
2020-09-25 16:40:06 +0800

    fix: re-register env.
    Move all register into vvlab.envs.__init__.py.
    vvlab.__init__ use import of vvlab.envs.__init__ for register.

--- vvlab/__init__.py
+++ vvlab/__init__.py
@@ -3,16 +3,8 @@
 """
 @author: Jiawei Wu
 @create time: 2019-11-25 10:49
-@edit time: 2020-03-30 10:12
+@edit time: 2020-09-25 16:16
 """
 
 from gym.envs.registration import register
-
-register(
-    id='FindTreasure-v0',
-    entry_point='vvlab.envs.findtreasure_env:FindTreasureEnv',
-)
-register(
-    id='Maze-v0',
-    entry_point='vvlab.envs.maze_env:MazeEnv',
-)
+import vvlab.envs
