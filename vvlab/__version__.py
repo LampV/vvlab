cd95d1d328bb9227046c37b97bf6ac71965f7886
wujiawei <1071952403@qq.com>
2020-09-25 16:43:05 +0800

    doc: update version

--- vvlab/__version__.py
+++ vvlab/__version__.py
@@ -3,12 +3,12 @@
 """
 @author: Jiawei Wu
 @create time: 2020-03-18 11:52
-@edit time: 2020-04-07 19:59
-@FilePath: /vvlab/__version__.py
+@edit time: 2020-09-25 16:42
+@FilePath: /vvlab/vvlab/__version__.py
 """
 
 
 
-VERSION = (0, 1, 9)
+VERSION = (0, 2, 0)
 
 __version__ = '.'.join(map(str, VERSION))
